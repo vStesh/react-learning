7d364fc9133d837532990fafd5439a66d8c2e911
vstesh <stesh@ukr.net>
2021-03-31 20:31:13 +0300

    t86

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -3,13 +3,14 @@ import style from './Profile.module.css';
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 import MyPostsContainer from "./MyPostsContainer";
 import ProfileStatus from "./ProfileStatus";
+import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 
 function Profile(props) {
 
     return (
         <div className="content">
             <ProfileInfo profile={props.profile}/>
-            <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
+            <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus}/>
             <div className={style.avatar}>{props.auth.login}</div>
             <MyPostsContainer />
         </div>
